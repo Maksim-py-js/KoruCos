2cfe47c10cede3acb602a065e9839dc0be55df63
Maksim-py-js <muhamedzyanov2004@gmail.com>
2020-09-12 14:14:37 +0500

    commit

---
+++ catalog-page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template name: Catalog page
+ */
+
+get_header();
+?>
+    <main class="catalog">
+        <div class="catalog__breadcrumbs">
+        <?php woocommerce_breadcrumb(); ?>
+        </div>
+        <div class="catalog__title">
+            Уход за лицом
+        </div>
+        <div class="catalog__filters"></div>
+    </main>
+<?php
+get_footer();
+?>
