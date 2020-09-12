b15d0d58a14a98b6b988782dcd915c776183007f
Maksim-py-js <muhamedzyanov2004@gmail.com>
2020-09-12 15:07:10 +0500

    commit

--- catalog-page.php
+++ catalog-page.php
@@ -12,7 +12,20 @@ get_header();
         <div class="catalog__title">
             Уход за лицом
         </div>
-        <div class="catalog__filters"></div>
+        <div class="catalog__filters">
+            <div class="catalog__filters-left">
+                <div class="dropdown">
+                    <span>Сортировать по</span>
+                    <div class="dropdown-content">
+                        <div class="item">Цена: убывание</div>
+                        <div class="item">Цена: возрастание</div>
+                        <div class="item">Название: А-Я</div>
+                        <div class="item">Название: Я-А</div>
+                    </div>
+                </div>
+            </div>
+            <div class="catalog__filters-right"></div>
+        </div>
     </main>
 <?php
 get_footer();
